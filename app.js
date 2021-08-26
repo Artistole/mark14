19a8d946298f66aa29dabc6545bb11ade0b9e475
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2021-08-26 21:35:26 +0530

    refactor

--- app.js
+++ app.js
@@ -3,18 +3,11 @@ var noOfStocks = document.querySelector('#stocks-quantity');
 var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
-var happy1 = document.querySelector('#happy');
 
 function submitHandler() {
     var ip = initialPrice.value;
     var qty = noOfStocks.value;
     var curr = currentPrice.value;
-
-        // e.preventDefault();
-        // cols[0].classList.remove("transperantBg");
-        // cols[1].classList.remove("transperantBg");
-        // contentDiv.classList.remove("sadTheme");
-        // contentDiv.classList.remove("happyTheme");
 
     calculateProfitAndLoss(ip, qty, curr);
 }
@@ -26,34 +19,34 @@ function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > 0 && current > 0 && quantity > 0) //no stock can ever have a initial price or current price as zero so >0 can be done here instead of writing a whole pls fill all the fields message
     {
         if (initial > current) {
-            var loss = (initial - current) * quantity;
-            var lossPercentage = (loss / initial) * 100;
-            var l = loss.toFixed(2);
-            var los = lossPercentage.toFixed(2);
+            var loss = ((initial - current) * quantity).toFixed(2);
+            var lossPercentage = ((loss / initial)* 100).toFixed(2);
+            // var l = loss.toFixed(2);
+            // var los = lossPercentage.toFixed(2);
 
-            showOutput(`The loss is ${l} and the loss percentage is ${los}%`)
-
-            // if(lossPer>50){
-            //     cols[0].classList.add("transperantBg");
-            //     cols[1].classList.add("transperantBg");
-            //     contentDiv.classList.add("sadTheme");
-            // }
+            showOutput(`The loss is ${loss} and the loss percentage is ${lossPercentage}%`)
+            if(lossPercentage>50)
+            document.body.style.backgroundImage = "URL('images/stonksdown.jpg')"
+            document.body.style.backgroundRepeat = "no-repeat";
+            document.body.style.backgroundSize = "cover";
+            document.body.style.backgroundPositionX ="0rem";
+            document.body.style.backgroundPositionY = "0rem";
 
         } else if (current > initial) {
             var profit = (current - initial) * quantity;
             var profitPercentage = (profit / initial) * 100;
             var p = profit.toFixed(2);
             var pro = profitPercentage.toFixed(2);
-
+            
             showOutput(`The profit is ${p} and the profit percentage is ${pro}% !`)
-            // if(profitPer>50){
-            //     cols[0].classList.add("transperantBg");
-            //     cols[1].classList.add("transperantBg");
-            //     contentDiv.classList.add("happyTheme");
-            //     }
-            happy1.style.display = "block";
+            document.body.style.backgroundImage = "URL('images/stonksup.jpg')";
+            document.body.style.backgroundRepeat = "no-repeat";
+            document.body.style.backgroundSize = "cover";
+            document.body.style.backgroundPositionX ="0rem";
+            document.body.style.backgroundPositionY = "0rem";
         } else if (current = initial) {
             showOutput("No loss")
+            document.body.style.backgroundColor = "#ffffff"
         }
     } else {
         showOutput("please fill all the fields")
